3d8a08bda960cfa8c142bfc9289373166af619c1
MaxLabLearn <maxlablearn@gmail.com>
2022-01-20 16:18:05 +0300

    feat: change js hamburger code

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,9 +1,19 @@
 const hamburgerLogo = document.querySelector('.hamburger');
-const hamburgerMenu = document.querySelector('.navigation');
+const hamburgerMenu = document.querySelector('.navigation')
+
 function toggleMenu() {
     hamburgerLogo.classList.toggle('open');
     hamburgerMenu.classList.toggle('show');
 }
+
+function closeMenu(event) {
+    if (event.target.classList.contains('nav-link')) {
+        hamburgerMenu.classList.remove('show');
+        hamburgerLogo.classList.remove('open');
+    }
+}
+
 hamburgerLogo.addEventListener('click', toggleMenu);
-hamburgerMenu.addEventListener('click', toggleMenu);
-console.log(`Вёрстка соответствует макету(+-10px). Ширина экрана 768px - 48\nНи на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки - 15\nНа ширине экрана 768рх и меньше реализовано адаптивное меню - 22\nСуммарно - 75`);
+hamburgerMenu.addEventListener('click', closeMenu);
+
+console.log(`Вёрстка соответствует макету (+-10px)на ширине экрана 768px - 48\nНи на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки - 15\nНа ширине экрана 768рх и меньше реализовано адаптивное меню - 22\nСуммарно - 75`);
