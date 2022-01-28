fa104a6a47c4f3441a649d674adc7b1b9af366a3
MaxLabLearn <maxlablearn@gmail.com>
2022-01-28 22:18:59 +0300

    feat: add change images on portfolio section

--- portfolio/index.js
+++ portfolio/index.js
@@ -14,8 +14,23 @@ function closeMenu(event) {
         document.body.classList.remove('lock');
     }
 }
-
 hamburgerLogo.addEventListener('click', toggleMenu);
 hamburgerMenu.addEventListener('click', closeMenu);
+
+const portfolioButton = document.querySelectorAll('.portfolio-button');
+const portfolioButtons = document.querySelector('.portfolio-buttons');
+const portfolioImages = document.querySelectorAll('.portfolio-image');
+
+function changeImage(event) {
+    if (event.target.classList.contains('portfolio-button')) {
+        portfolioImages.forEach((img, index) => img.src = `./assets/img/${event.target.dataset.season}/${index + 1}.jpg`);
+        portfolioButton.forEach((elem) => elem.classList.remove('active'));
+        event.target.classList.add('active');
+    }
+}
+
+portfolioButtons.addEventListener('click', changeImage);
+
+
 
 console.log(`Вёрстка соответствует макету (+-10px)на ширине экрана 768px - 48\nНи на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки - 15\nНа ширине экрана 768рх и меньше реализовано адаптивное меню - 22\nСуммарно - 75`);
