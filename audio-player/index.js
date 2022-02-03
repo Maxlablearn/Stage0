2d9718a3eb4702126a5275fb945e44c1dee6e025
MaxLabLearn <maxlablearn@gmail.com>
2022-02-03 22:22:50 +0300

    fead: add animated play/pause button

--- audio-player/index.js
+++ audio-player/index.js
@@ -1,1 +1,17 @@
 //
+let isPlay = false;
+
+const playButton = document.querySelector('.play-btn');
+const playButtonBars = document.querySelectorAll('.bar');
+
+function playPause() {
+  isPlay = !isPlay;
+  console.log('bums');
+  if (isPlay) {
+    playButtonBars.forEach(el => el.classList.add('pause'));
+  } else {
+    playButtonBars.forEach(el => el.classList.remove('pause'));
+  }
+}
+
+playButton.addEventListener('click', playPause);
