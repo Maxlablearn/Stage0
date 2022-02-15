942b21c85a9e03db2f9678c11afeeba08a80e510
MaxLabLearn <maxlablearn@gmail.com>
2022-02-15 20:12:37 +0300

    feat: add throw functions for whole field and directions

--- random-game/index.js
+++ random-game/index.js
@@ -3,13 +3,14 @@ let field = Array(16).fill('');
 const colors = {2: 'lightgreen', 4: 'green', 8: 'orange', 16: 'red',
               32: 'sandybrown', 64: 'braun', 128: 'violet', 256: 'purple',
               512: 'goldenrod', 1024: 'yellow', 2048: 'gold', 4096: 'deeppink'};
+let isThrow = false;
 
 
 const body = document.getElementsByTagName('body');
 const items = document.querySelectorAll('[data-item]');
 
-console.log(body);
-console.log(items);
+//console.log(body);
+//console.log(items);
 
 
 
@@ -24,20 +25,62 @@ function getFreeItems() {
   return result;  
 }
 
-function action(event) {
-  insertRandom(getFreeItems());
-  console.log(field);
-  render();
+function getColumn(num) {
+  return [field[num], field[num+4], field[num+8] ,field[num+12]];
+}  
+function getRow(num) {
+  return [field[num*4], field[num*4+1], field[num*4+2], field[num*4+3]];
+}
+function setColumn(arr, num) {
+  [field[num], field[num+4], field[num+8] ,field[num+12]] = arr;
+}
+function setRow(arr, num) {
+  [field[num*4], field[num*4+1], field[num*4+2], field[num*4+3]] = arr;
 }
 
-function throw(dir) {
-  let isThrow = false;
-  let rows = [field[0],field[1], fi]
-  if (dir === 'up') {
-    for (let i = 0; i < 4; i++) {
-      
+function throwOne(arr) {
+  let j = 0;
+  isThrow = false;
+  let result = ['','','','']; 
+  for (let i = 0 ; i < 4; i ++) {
+    if (arr[i] > 1) {
+      if (result[j-1] === arr[i]) {
+        isThrow = true;
+        result[j-1] = arr[i] * 2;
+      } else {
+        result[j] = arr[i];
+        j++;
+      }
     }
   }
+  console.log(result);
+  return result;
+}
+
+function throwAll(dir) {
+  switch (dir) {
+    case 'ArrowUp':
+      for (let i = 0; i < 4; i++) {
+        setColumn(throwOne(getColumn(i)), i);
+      };
+      break;
+    case 'ArrowDown':
+      for (let i = 0; i < 4; i++) {
+        setColumn(throwOne(getColumn(i).reverse()).reverse(), i);
+      };
+      break;
+    case 'ArrowLeft':
+      for (let i = 0; i < 4; i++) {
+        setRow(throwOne(getRow(i)), i);
+      }
+      break;
+    case 'ArrowRight':
+      for (let i = 0; i < 4; i++) {
+        setRow(throwOne(getRow(i).reverse()).reverse(),i);
+      }
+      break;
+  } 
+  
 }
 
 function render() {
@@ -47,6 +90,24 @@ function render() {
   });
 }
 
+function actionKey(event) {
+  throwAll(event.key);
+  
+  //setColumn(throwOne(getColumn(1)), 1);
+  console.log(event);
+  render();
+  setTimeout( () => {
+    if (getFreeItems().length > 0) {
+      insertRandom(getFreeItems());
+    } else {
+      alert('game over');
+    }
+    render();
+  }, 500)
+  
+}
 
-document.body.addEventListener('keyup', action);
-console.log(getFreeItems());
+
+
+document.body.addEventListener('keyup', actionKey);
+//console.log(getFreeItems());
